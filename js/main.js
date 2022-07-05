920886d1e41e1a9676062ad4b96d6ec7139ee2a2
Ara Kanic <ji.arakani@gmail.com>
2022-07-04 21:04:18 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,14 @@
-/*
-// Listing HTML
-			<section class="drink-item">
-				<section class="description">
-					<h2>Name</h2>
-					<img src="" alt="">		
-				</section>
-				<section class="ingredients">
-					<h3>Ingredients</h3>
-                    <ul><ul>
-				</section>
-				<section class="instructions">
-					<h4>Instructions</h4>
-				</section>
-			</section>
-*/
-const sectionDrinks = document.querySelector('.drinks')
-const h3Error = document.querySelector(".error")
+let results = document.querySelector('.results')
+let cardIndex = 0
 
 document.querySelector('button').addEventListener('click', getDrink)
 
 function getDrink() {
+    // Clear existing elements inside drinks section
+    removeAllChildNodes(results)
+    // User query thecocktaildb.com
     let userInput = document.querySelector('input').value
     const inputFormatted = userInput.split(' ').join('-')
-    // console.log(inputFormatted)
     const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputFormatted}`
 
     fetch(url)
@@ -30,27 +16,48 @@ function getDrink() {
         .then(data => {
             // Data object w/ "drinks" property containing ARRAY of drink objects
             let drinks = data['drinks']
-            // for each drink... create listing w/ relevant drink information
+
+            // Slide control div
+            let controlPanel = document.createElement("div")
+            controlPanel.className = "control-panel"
+
+            let buttonPrevious = document.createElement("button")
+            buttonPrevious.className = "control"
+            buttonPrevious.setAttribute('id', "previous")
+            let chevronLeft = document.createElement("i")
+            chevronLeft.className = "fa-solid fa-chevron-left"
+            buttonPrevious.appendChild(chevronLeft)
+
+            let buttonNext = document.createElement("button")
+            buttonNext.className = "control"
+            buttonNext.setAttribute('id', "next")
+            let chevronRight = document.createElement("i")
+            chevronRight.className = "fa-solid fa-chevron-right"
+            buttonNext.appendChild(chevronRight)
+
+            controlPanel.appendChild(buttonPrevious)
+            controlPanel.appendChild(buttonNext)
+
+            // Drinks div(s)
+            let divDrinks = document.createElement("div")
+            divDrinks.className = "drinks slideshow"
+
+            // create card div w/ relevant info per drink
             // -> create DOM elements
             for (let index = 0; index < drinks.length; index++) {
-                let sectionItem = document.createElement("section")
-                sectionItem.className = "drink-item"
-                // Description section
-                let sectionDesc = document.createElement("section")
-                sectionDesc.className = "description"
-                let h2Elem = document.createElement("h2")
-                h2Elem.innerText = drinks[index].strDrink
-                sectionDesc.appendChild(h2Elem)
-                let imgElem = document.createElement("img")
-                imgElem.src = drinks[index].strDrinkThumb
-                sectionDesc.appendChild(imgElem)
-                sectionItem.appendChild(sectionDesc)
-                // Description Ingredients
-                let sectionIng = document.createElement("section")
-                sectionIng.className = "ingredients"
-                let h3Elem = document.createElement("h3")
-                h3Elem.innerText = "Ingredients"
-                sectionIng.appendChild(h3Elem)
+                let card = document.createElement("div")
+                card.className = index != 0 ? "card" : "card show"
+                // drink image in thumb div
+                let thumb = document.createElement("div")
+                thumb.className = "thumb"
+                let thumbURL = drinks[index].strDrinkThumb
+                thumb.style.setProperty('background-image', `url('${thumbURL}')`)
+                // drink info in article element
+                article = document.createElement("article")
+                let h1Elem = document.createElement("h1")
+                h1Elem.innerText = drinks[index].strDrink
+                article.appendChild(h1Elem)
+                // Ingredients list
                 let ulIng = document.createElement("ul")
                 let objIngredients = {}
                 // parse drink keys with associated properties to find, set ingredients
@@ -76,19 +83,50 @@ function getDrink() {
                         ulIng.appendChild(liNew)
                     }
                 }
-                sectionIng.appendChild(ulIng)
-                sectionItem.appendChild(sectionIng)
-                // Instructions section
-                let sectionInstr = document.createElement("section")
-                sectionInstr.className = "instructions"
-                let h4Elem = document.createElement("h4")
-                h4Elem.innerText = drinks[index].strInstructions
-                sectionInstr.appendChild(h4Elem)
-                sectionItem.appendChild(sectionInstr)
-                // Append sections to DOM
-                sectionDrinks.appendChild(sectionItem)
-            }})
-        .catch(err => {
-            h3Error.innerText = `No ${userInput}s today! Try something else...`
+                article.appendChild(ulIng)
+                // Instructions
+                let pInstructions = document.createElement("p")
+                pInstructions.innerText = drinks[index].strInstructions
+                article.appendChild(pInstructions)
+                // Append image, info elements to card div
+                card.appendChild(thumb)
+                card.appendChild(article)
+                divDrinks.appendChild(card)
+            }
+            // Add click event listeners
+            buttonPrevious.addEventListener('click', previousCard)
+            buttonNext.addEventListener('click', nextCard)            
+            // Append drinks div to results section
+            results.appendChild(controlPanel)
+            results.appendChild(divDrinks)
         })
+        .catch(err => {
+            let h3Error = document.createElement("h3")
+            h3Error.className = "error"
+            h3Error.innerText = `No ${userInput}s today! You can searching for something else.`
+            results.appendChild(h3Error)
+        })
+}
+
+// Card slide functions
+function goToCard(n) {
+  let cards = document.querySelectorAll('.card')
+  cards[cardIndex].className = 'card'
+  cardIndex = (n + cards.length) % cards.length
+  cards[cardIndex].className = 'card show'
+}
+
+function nextCard() {
+    goToCard(cardIndex + 1)
+}
+
+function previousCard() {
+    goToCard(cardIndex - 1)
+}
+
+// Remove elements from results section
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
